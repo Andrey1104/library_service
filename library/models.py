258880fe86f9440c3d944bcf8c7f3116f167b6b0
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-04-26 16:00:59 +0300

    added borrowing model

--- library/models.py
+++ library/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import Count, F, ExpressionWrapper, IntegerField, Q
+
+from library_service.settings import AUTH_USER_MODEL
 
 
 class Book(models.Model):
@@ -18,3 +21,32 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def free(self):
+        available_books_count = (
+            Book.objects.annotate(
+                borrowed_count=Count(
+                    "borrowing",
+                    filter=~Q(borrowing__actual_return_date__isnull=True)
+                )
+            )
+            .annotate(
+                available_inventory=ExpressionWrapper(
+                    F("inventory") - F("borrowed_count"),
+                    output_field=IntegerField()
+                )
+            )
+            .values_list("title", "available_inventory")
+        )
+        return available_books_count.filter(
+            title=self.title,
+            author=self.author
+        ).values_list("available_inventory", flat=True).first()
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(auto_now=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
