f34780ca05bc0707454042a3e74b674a35682ec5
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-04-26 16:39:40 +0300

    added borrowing view

--- library/views.py
+++ library/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from library.models import Book
-from library.serializers import BookSerializer
+from library.models import Book, Borrowing
+from library.serializers import BookSerializer, BorrowingSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
