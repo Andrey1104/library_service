334ff7cae4cd04385dcf14f4921adbb3ba15b7e9
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-04-30 18:03:39 +0300

    added swagger documentation

--- library_service/urls.py
+++ library_service/urls.py
@@ -16,9 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("library.urls", namespace="library")),
-    path("api/user", include("user.urls", namespace="user"))
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/payment/", include("payment.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger"
+    ),
 ]
