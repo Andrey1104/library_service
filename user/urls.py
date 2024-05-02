95523e43aa43c97c29e71b3876f3c641b24e6ec0
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-05-02 23:52:01 +0300

    added telegram url path

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView, TokenRefreshView, TokenObtainPairView
 
-from user.views import ManageUserView, CreateUserView
+from user.views import ManageUserView, CreateUserView, UserTelegramView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
@@ -9,6 +9,7 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("telegram/", UserTelegramView.as_view(), name="telegram")
 ]
 
 app_name = "user"
