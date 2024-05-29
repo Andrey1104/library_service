857d5ce22b7ebeafb881f676a2c4568b7ab4ad4e
Andrey1104 <malovaniy.an@gmail.com>
2024-05-29 22:23:00 +0200

    fix session_url max_length in model

--- payment/models.py
+++ payment/models.py
@@ -7,6 +7,6 @@ class Payment(models.Model):
     status = models.CharField(max_length=10, choices=[('PENDING', 'Pending'), ('PAID', 'Paid')])
     type = models.CharField(max_length=10, choices=[('PAYMENT', 'Payment'), ('FINE', 'Fine')])
     borrowing = models.OneToOneField(Borrowing, on_delete=models.CASCADE)
-    session_url = models.URLField()
+    session_url = models.URLField(max_length=500)
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2, default=0)
